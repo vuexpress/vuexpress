b3a12fb8d555ad26a521b7fc4a4a57d20b1aff1d
do-web <dweber@uandi.com>
2018-02-03 22:19:37 +0100

    Init

---
+++ connect.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const rendererFactory = require('./renderer/factory');
+const ErrorTypes = require('./error');
+
+const noop = () => {
+};
+
+function vueRenderer(baseOptions) {
+    const errorHandler = (e) => {
+        if (baseOptions && baseOptions.onError) {
+            baseOptions.onError(e);
+        } else {
+            console.error(e); // eslint-disable-line no-console
+        }
+    };
+    const readyHandler = (baseOptions && baseOptions.onReady) || noop;
+    const basePath = baseOptions.views;
+    const renderer = rendererFactory(basePath, baseOptions);
+    renderer.on('error', errorHandler);
+    renderer.on('ready', readyHandler);
+
+    return (req, res, next) => {
+        const url = req.originalUrl;
+        res.render = (vueFilePath, state, options) => {
+
+            vueFilePath = vueFilePath + '.vue';
+
+            res.set('Content-Type', 'text/html');
+            const filePath = path.resolve(basePath, vueFilePath);
+            const renderOptions = Object.assign({}, {url}, options);
+            return renderer.renderToStream(filePath, state, renderOptions).then((stream) => {
+                stream.on('data', chunk => res.write(chunk));
+                stream.on('end', () => res.end());
+            }).catch((e) => {
+                const error = new ErrorTypes.RenderError(e);
+                error.component = vueFilePath;
+                error.state = state;
+                errorHandler(error);
+                next(error);
+            });
+        };
+        res.renderToStream = (vueFilePath, state, options) => {
+            const filePath = path.resolve(basePath, vueFilePath);
+            const renderOptions = Object.assign({}, {url}, options);
+            return renderer.renderToStream(filePath, state, renderOptions);
+        };
+        res.renderToString = (vueFilePath, state, options) => {
+            const renderOptions = Object.assign({}, {url}, options);
+            const filePath = path.resolve(basePath, vueFilePath);
+            return renderer.renderToString(filePath, state, renderOptions);
+        };
+        return next();
+    };
+}
+
+module.exports = vueRenderer;
---
+++ renderer/factory.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const MemoryFS = require('memory-fs');
+const Compiler = require('../renderer/compiler');
+const Renderer = require('../renderer/renderer');
+
+const mfs = new MemoryFS();
+
+const defaultOptions = {
+    head: {},
+    compilerConfig: {},
+    preCompile: [],
+    plugins: [],
+    watch: false,
+    outputPath: '/tmp/vuexpress_ssr',
+    global: Object.create(null),
+};
+
+/**
+ * Get renderer
+ *
+ * @param {string} basePath
+ * @param {Object} vOptions
+ * @returns
+ */
+function rendererFactory(basePath, vOptions) {
+    const options = Object.assign({}, defaultOptions, vOptions);
+    options.preCompile = options.preCompile.map(filePath => path.resolve(basePath, filePath));
+
+    const compilerOptions = {
+        config: options.compilerConfig,
+        basePath: path.resolve(basePath),
+        watch: options.watch,
+        globals: options.globals,
+        outputPath: options.outputPath,
+        cssOutputPath: options.cssOutputPath,
+        publicPath: options.publicPath,
+    };
+    const compiler = new Compiler(mfs, compilerOptions);
+
+    const rendererOptions = {
+        head: options.head,
+        plugins: options.plugins,
+        preCompile: options.preCompile,
+        globals: options.globals,
+    };
+    const renderer = new Renderer(compiler, rendererOptions);
+
+    return renderer;
+}
+
+module.exports = rendererFactory;
