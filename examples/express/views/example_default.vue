22ddca3b6160cb6efbba4ff7a0691d88b322f687
do-web <dweber@uandi.com>
2018-05-19 00:43:06 +0200

    Add webpack 4

--- examples/express/views/example_default.vue
+++ examples/express/views/example_default.vue
@@ -1,45 +1,33 @@
 <template>
-    <div>
-        {{myVar}} {{example}}
-    </div>
+  <div>
+    {{myVar}} {{example}}
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Example default',
-        data() {
-            return {
-                example: 'Hello World 123',
-            };
-        },
-        metaInfo: {
-            title: 'Default Title',
-            titleTemplate: '%s | My Awesome Website'
-        },
-        created() {},
-        methods: {},
-        computed: {},
-        components: {}
-    }
+  export default {
+    name: 'Example default',
+    data() {
+      return {
+        example: 'Hello World 123',
+      };
+    },
+    metaInfo: {
+      title: 'Default Title',
+      titleTemplate: '%s | My Awesome Website'
+    },
+    created() {},
+    methods: {},
+    computed: {},
+    components: {}
+  }
 </script>
 
 <style lang="scss">
-    body {
-
-            font-size: 16px;
-            font-weight: bold;
-      color: black;
-
+  body {
+    #app {
+      font-size: 16px;
+      font-weight: bold;
     }
-    :root {
-      --red: #d33;
-    }
-    a {
-      &:hover {
-        color: color(var(--red) a(54%));
-      }
-    }
-  ::placeholder {
-    color:red;
   }
 </style>
